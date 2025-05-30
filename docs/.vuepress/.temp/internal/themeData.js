a833948beb09ce2c7c2b94e78c478a002ed18cfb
Stephanie Fuda <stephanie@perispheremedia.com>
2025-05-30 12:23:38 -0400

    NPM update

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,1 +1,14 @@
 export const themeData = JSON.parse("{\"logo\":\"StephanieCodes-Logo-2.png\",\"navbar\":[\"/\",\"/projects\"],\"repo\":\"https://github.com/StephanieF\",\"editLink\":false,\"lastUpdated\":true,\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebar\":\"heading\",\"sidebarDepth\":2,\"editLinkText\":\"Edit this page\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateThemeData) {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ themeData }) => {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  })
+}
