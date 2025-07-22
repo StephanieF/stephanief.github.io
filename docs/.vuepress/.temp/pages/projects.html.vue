679af9a064d19b89433e8b306ac53b9de5515a5f
Stephanie Fuda <stephaniefuda@proton.me>
2025-07-22 09:00:03 -0400

    adding new project to site

--- docs/.vuepress/.temp/pages/projects.html.vue
+++ docs/.vuepress/.temp/pages/projects.html.vue
@@ -2,6 +2,10 @@
 <p>Personal software engineering projects allow me to practice technical skills, problem-solving abilities, and creativity by experimenting with languages and products that I may not use during the execution of my daily work.</p>
 <p>Everything is open source; please fork the work or comment back; the best code is collaborative.</p>
 <h2 id="_2025" tabindex="-1"><a class="header-anchor" href="#_2025"><span>2025</span></a></h2>
+<h4 id="synthwave-ipsum" tabindex="-1"><a class="header-anchor" href="#synthwave-ipsum"><span>Synthwave Ipsum</span></a></h4>
+<p>As an experiment in &quot;Vibe Coding&quot; I've created a Lorem Ipsum generator, with a goal of keeping the time and cost as low as possible.</p>
+<p>The secondary goal was to start creating medium posts as a way to share knowledge and practice writing skills.</p>
+<p><a href="https://loremipsumawesome.com/" target="_blank" rel="noopener noreferrer">Lorem Ipsum Awesome</a>  <a href="https://github.com/StephanieF/synthwave-lorem" target="_blank" rel="noopener noreferrer">Repo</a>  <a href="https://medium.com/@stephaniefuda/from-zero-to-deployed-my-first-vibe-coding-experiment-cbf533c5f609" target="_blank" rel="noopener noreferrer">Medium Post</a></p>
 <h4 id="visual-detective" tabindex="-1"><a class="header-anchor" href="#visual-detective"><span>Visual Detective</span></a></h4>
 <p>Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools
 <a href="https://chatgpt.com/g/g-DhaGfosbH-visual-detective" target="_blank" rel="noopener noreferrer">Visual Detective</a></p>
--- docs/projects.md
+++ docs/projects.md
@@ -6,6 +6,15 @@ Everything is open source; please fork the work or comment back; the best code i
 
 ## 2025
 
+#### Synthwave Ipsum
+As an experiment in "Vibe Coding" I've created a Lorem Ipsum generator, with a goal of keeping the time and cost as low as possible.
+
+The secondary goal was to start creating medium posts as a way to share knowledge and practice writing skills.
+
+
+[Lorem Ipsum Awesome](https://loremipsumawesome.com/)  [Repo](https://github.com/StephanieF/synthwave-lorem)  [Medium Post](https://medium.com/@stephaniefuda/from-zero-to-deployed-my-first-vibe-coding-experiment-cbf533c5f609)
+
+
 #### Visual Detective
 Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools
 [Visual Detective](https://chatgpt.com/g/g-DhaGfosbH-visual-detective)
