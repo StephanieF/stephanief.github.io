ff6d75a460845aca92079d6a762ceed11ca07b7c
Stephanie Fuda <stephaniefuda@proton.me>
2025-07-22 09:00:32 -0400

    compile site

--- docs/.vuepress/.temp/pages/personal-site.html.js
+++ docs/.vuepress/.temp/pages/personal-site.html.js
@@ -1,16 +1,3 @@
 import comp from "/home/stephaniefuda/projects/stephanief.github.io/docs/.vuepress/.temp/pages/personal-site.html.vue"
 const data = JSON.parse("{\"path\":\"/personal-site.html\",\"title\":\"Personal Site\",\"lang\":\"en-US\",\"frontmatter\":{\"description\":\"Personal Site This is a normal page, which contains VuePress basics. Pages You can add markdown files in your vuepress directory, every markdown file will be converted to a page...\",\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"Personal Site\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":\\\"2024-09-01T21:08:59.000Z\\\",\\\"author\\\":[]}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.stephaniecod.es/personal-site.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"Stephanie Fuda\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Personal Site\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Personal Site This is a normal page, which contains VuePress basics. Pages You can add markdown files in your vuepress directory, every markdown file will be converted to a page...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2024-09-01T21:08:59.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2024-09-01T21:08:59.000Z\"}]]},\"git\":{\"updatedTime\":1725224939000,\"contributors\":[{\"name\":\"Stephanie Fuda\",\"username\":\"\",\"email\":\"stephaniefuda@stephaniecod.es\",\"commits\":1}],\"changelog\":[{\"hash\":\"bb9c747c70c0508177c5046ccea05fb374070c82\",\"time\":1725224939000,\"email\":\"stephaniefuda@stephaniecod.es\",\"author\":\"Stephanie Fuda\",\"message\":\"initial commit of rebuilt repo\"}]},\"filePathRelative\":\"personal-site.md\",\"autoDesc\":true}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
