61a63980ac5fbaf84c9186ba006969de6bdcfdd7
Stephanie Fuda <stephaniefuda@proton.me>
2025-11-30 20:28:05 -0500

    NPM update

--- docs/.vuepress/.temp/pages/projects.html.js
+++ docs/.vuepress/.temp/pages/projects.html.js
@@ -1,3 +1,16 @@
 import comp from "/home/stephaniefuda/projects/stephanief.github.io/docs/.vuepress/.temp/pages/projects.html.vue"
 const data = JSON.parse("{\"path\":\"/projects.html\",\"title\":\"Projects\",\"lang\":\"en-US\",\"frontmatter\":{\"description\":\"Projects Personal software engineering projects allow me to practice technical skills, problem-solving abilities, and creativity by experimenting with languages and products tha...\",\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"Projects\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":\\\"2025-08-06T22:21:33.000Z\\\",\\\"author\\\":[]}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.stephaniecod.es/projects.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"Stephanie Fuda\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Projects\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Projects Personal software engineering projects allow me to practice technical skills, problem-solving abilities, and creativity by experimenting with languages and products tha...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2025-08-06T22:21:33.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2025-08-06T22:21:33.000Z\"}]]},\"git\":{\"updatedTime\":1754518893000,\"contributors\":[{\"name\":\"Stephanie Fuda\",\"username\":\"\",\"email\":\"stephaniefuda@stephaniecod.es\",\"commits\":6}],\"changelog\":[{\"hash\":\"80b341afee8df48f9e08433bd8fa01b3a38fbbd9\",\"time\":1754518893000,\"email\":\"stephaniefuda@proton.me\",\"author\":\"Stephanie Fuda\",\"message\":\"adding about page and styling\"},{\"hash\":\"679af9a064d19b89433e8b306ac53b9de5515a5f\",\"time\":1753189203000,\"email\":\"stephaniefuda@proton.me\",\"author\":\"Stephanie Fuda\",\"message\":\"adding new project to site\"},{\"hash\":\"344a6c7194d27e5d1160dc665a57fed31fedc294\",\"time\":1741643889000,\"email\":\"stephaniefuda@stephaniecod.es\",\"author\":\"Stephanie Fuda\",\"message\":\"updating portfolio and adding contact info\"},{\"hash\":\"b531c7107b5a7c60f968f27a7f268625ae6231f7\",\"time\":1730309327000,\"email\":\"stephaniefuda@stephaniecod.es\",\"author\":\"Stephanie Fuda\",\"message\":\"fixing end tag\"},{\"hash\":\"449f729ed2e79df397b30b6505ae5fa065b1a673\",\"time\":1730227978000,\"email\":\"stephaniefuda@stephaniecod.es\",\"author\":\"Stephanie Fuda\",\"message\":\"text tweaks\"},{\"hash\":\"bb9c747c70c0508177c5046ccea05fb374070c82\",\"time\":1725224939000,\"email\":\"stephaniefuda@stephaniecod.es\",\"author\":\"Stephanie Fuda\",\"message\":\"initial commit of rebuilt repo\"}]},\"filePathRelative\":\"projects.md\",\"autoDesc\":true}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
