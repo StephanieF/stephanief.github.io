710a6b0273cc3166e3fe75911c9c32d8b57f4ad8
Stephanie Fuda <stephaniefuda@proton.me>
2025-12-09 18:10:55 -0500

    site content changes

--- docs/.vuepress/.temp/pages/projects.html.vue
+++ docs/.vuepress/.temp/pages/projects.html.vue
@@ -6,8 +6,9 @@
 <p>As an experiment in &quot;Vibe Coding&quot; I've created a Lorem Ipsum generator, with a goal of keeping the time and cost as low as possible.</p>
 <p>The secondary goal was to start creating medium posts as a way to share knowledge and practice writing skills.</p>
 <p><a href="https://loremipsumawesome.com/" target="_blank" rel="noopener noreferrer">Lorem Ipsum Awesome</a>  <a href="https://github.com/StephanieF/synthwave-lorem" target="_blank" rel="noopener noreferrer">Repo</a>  <a href="https://medium.com/@stephaniefuda/from-zero-to-deployed-my-first-vibe-coding-experiment-cbf533c5f609" target="_blank" rel="noopener noreferrer">Medium Post</a></p>
-<h4 id="visual-detective" tabindex="-1"><a class="header-anchor" href="#visual-detective"><span>Visual Detective</span></a></h4>
-<p>Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools
+<h3 id="visual-detective" tabindex="-1"><a class="header-anchor" href="#visual-detective"><span>Visual Detective</span></a></h3>
+<p>Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools.</p>
+<p>As an experiment in building tools for the ChatGPT ecosphere I've created an image review tool that let's you know if an image has been maniputlated by AI anhow/where those changes have been made to the image.
 <a href="https://chatgpt.com/g/g-DhaGfosbH-visual-detective" target="_blank" rel="noopener noreferrer">Visual Detective</a></p>
 <h2 id="_2024" tabindex="-1"><a class="header-anchor" href="#_2024"><span>2024</span></a></h2>
 <h3 id="personal-site" tabindex="-1"><a class="header-anchor" href="#personal-site"><span>Personal Site</span></a></h3>
--- docs/projects.md
+++ docs/projects.md
@@ -14,9 +14,11 @@ The secondary goal was to start creating medium posts as a way to share knowledg
 
 [Lorem Ipsum Awesome](https://loremipsumawesome.com/)  [Repo](https://github.com/StephanieF/synthwave-lorem)  [Medium Post](https://medium.com/@stephaniefuda/from-zero-to-deployed-my-first-vibe-coding-experiment-cbf533c5f609)
 
-#### Visual Detective
+### Visual Detective
 
-Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools
+Images manipulated by AI generation can be hard to spot by simply looking. This tool highlights and explains the changes made by AI tools.
+
+As an experiment in building tools for the ChatGPT ecosphere I've created an image review tool that let's you know if an image has been maniputlated by AI anhow/where those changes have been made to the image.
 [Visual Detective](https://chatgpt.com/g/g-DhaGfosbH-visual-detective)
 
 ## 2024
